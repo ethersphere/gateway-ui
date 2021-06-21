7a91bb53307634cf2a498e834a4573bf16e35204
Vojtech Simetka <vojtech@simetka.cz>
2021-06-21 13:04:51 +0200

    fix: terms and conditions alert

--- src/components/TermsAndConditionsPopup.tsx
+++ src/components/TermsAndConditionsPopup.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState } from 'react'
+import type { ReactElement } from 'react'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
@@ -32,107 +32,103 @@ const useStyles = makeStyles((theme: Theme) =>
       justifyContent: 'space-between',
     },
     ul: {
-      display: 'flex',
-      flexDirection: 'column',
+      display: 'grid',
+      gridAutoRows: 'auto',
+      gridTemplateColumns: '1rem 1fr',
+      gridTemplateRows: 'min-content',
       paddingTop: theme.spacing(3),
       paddingBottom: theme.spacing(3),
       rowGap: theme.spacing(1),
+      columnGap: theme.spacing(2),
+      textAlign: 'left',
     },
     paper: {
       padding: theme.spacing(3),
     },
     li: {
-      textAlign: 'left',
-      paddingLeft: '1.3rem',
-      textIndent: '-1.25rem',
-      '&::before': {
-        content: '"🐝"',
-      },
+      display: 'flex',
+      alignItems: 'center',
     },
   }),
 )
 
-export default function TermsAndConditionsPopup(): ReactElement | null {
+interface Props {
+  handleAgree: () => void
+}
+
+export default function TermsAndConditionsPopup({ handleAgree }: Props): ReactElement | null {
   const classes = useStyles()
   const history = useHistory()
 
-  const [agreedToTerms, setAgreedToTerms] = useState(Boolean(window.localStorage.getItem('agreedToTerms')))
-
-  const handleAgree = () => {
-    setAgreedToTerms(true)
-    window.localStorage.setItem('agreedToTerms', Date.now().toString())
-  }
-
-  if (agreedToTerms) return null
-
   return (
-    <div className={classes.root}>
-      <Layout
-        top={[
-          <Header
-            key="top1"
-            rightAction={
-              <IconButton
-                onClick={() => {
-                  history.push(ROUTES.LANDING_PAGE)
-                }}
-              >
-                <X />
-              </IconButton>
-            }
-          >
-            {text.termsAndConditions.header}
-          </Header>,
-          <Typography key="top2" variant="subtitle1">
-            {text.termsAndConditions.tagline1}{' '}
-            <Link href={ROUTES.TERMS_AND_CONDITIONS} color="inherit" underline="always" target="blank">
-              {text.termsAndConditions.termsAndConditions}
-            </Link>{' '}
-            {text.termsAndConditions.tagline2}
-          </Typography>,
-        ]}
-        center={[
-          <div key="center">
-            <Paper square elevation={0} className={classes.paper}>
-              <Typography variant="subtitle1">{text.termsAndConditions.featuresAndLimitationsHeader}</Typography>
-              <div className={classes.ul}>
-                {text.termsAndConditions.featuresAndLimitations.map(t => (
-                  <Typography key={t} className={classes.li} variant="body1">
-                    {t}
-                  </Typography>
-                ))}
-              </div>
-              <Typography variant="body2">
-                {text.termsAndConditions.disclaimer1}{' '}
-                <Link href={ROUTES.TERMS_AND_CONDITIONS} color="inherit" underline="always" target="blank">
-                  {text.termsAndConditions.disclaimer2}
-                </Link>
-              </Typography>
-            </Paper>
-            <Button
-              className={classes.button}
-              size="small"
-              style={{ marginTop: 2, paddingLeft: 16, paddingRight: 16 }}
-              onClick={() => history.push(ROUTES.LANDING_PAGE)}
+    <Layout
+      top={[
+        <Header
+          key="top1"
+          rightAction={
+            <IconButton
+              onClick={() => {
+                history.push(ROUTES.LANDING_PAGE)
+              }}
             >
-              <CornerUpLeft />
-              {text.accessPage.backAction}
-              <CornerUpLeft style={{ opacity: 0 }} />
+              <X />
+            </IconButton>
+          }
+        >
+          {text.termsAndConditions.header}
+        </Header>,
+        <Typography key="top2" variant="subtitle1">
+          {text.termsAndConditions.tagline1}{' '}
+          <Link href={ROUTES.TERMS_AND_CONDITIONS} color="inherit" underline="always" target="blank">
+            {text.termsAndConditions.termsAndConditions}
+          </Link>{' '}
+          {text.termsAndConditions.tagline2}
+        </Typography>,
+      ]}
+      center={[
+        <div key="center" style={{ overflowY: 'scroll' }}>
+          <Paper square elevation={0} className={classes.paper}>
+            <Typography variant="subtitle1">{text.termsAndConditions.featuresAndLimitationsHeader}</Typography>
+            <div className={classes.ul}>
+              {text.termsAndConditions.featuresAndLimitations.map(t => [
+                <Typography key={`${t}-bee`} className={classes.li} variant="body1">
+                  🐝
+                </Typography>,
+                <Typography key={`${t}-text`} className={classes.li} variant="body1">
+                  {t}
+                </Typography>,
+              ])}
+            </div>
+            <Typography variant="body2">
+              {text.termsAndConditions.disclaimer1}{' '}
+              <Link href={ROUTES.TERMS_AND_CONDITIONS} color="inherit" underline="always" target="blank">
+                {text.termsAndConditions.disclaimer2}
+              </Link>
+            </Typography>
+          </Paper>
+          <Button
+            className={classes.button}
+            size="small"
+            style={{ marginTop: 2, paddingLeft: 16, paddingRight: 16 }}
+            onClick={() => history.push(ROUTES.LANDING_PAGE)}
+          >
+            <CornerUpLeft />
+            {text.accessPage.backAction}
+            <CornerUpLeft style={{ opacity: 0 }} />
+          </Button>
+        </div>,
+      ]}
+      bottom={[
+        <div key="bottom1" style={{ zIndex: 1000 }}>
+          <Footer>
+            <Button className={classes.button} size="large" onClick={handleAgree}>
+              <Check />
+              {text.termsAndConditions.agreeAction}
+              <Check style={{ opacity: 0 }} />
             </Button>
-          </div>,
-        ]}
-        bottom={[
-          <div key="bottom1" style={{ zIndex: 1000 }}>
-            <Footer>
-              <Button className={classes.button} size="large" onClick={handleAgree}>
-                <Check />
-                {text.termsAndConditions.agreeAction}
-                <Check style={{ opacity: 0 }} />
-              </Button>
-            </Footer>
-          </div>,
-        ]}
-      />
-    </div>
+          </Footer>
+        </div>,
+      ]}
+    />
   )
 }
