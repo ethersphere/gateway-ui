5affb7b2908bca3b3a758b4a69b70546d6d373da
Vojtech Simetka <vojtech@simetka.cz>
2021-06-24 13:06:14 +0200

    chore: removed unused import

--- src/components/InvalidSwarmHash.tsx
+++ src/components/InvalidSwarmHash.tsx
@@ -1,6 +1,5 @@
 import type { ReactElement } from 'react'
 import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
 import { AlertOctagon } from 'react-feather'
 import text from '../translations'
 import LayoutContent from './LayoutContent'
