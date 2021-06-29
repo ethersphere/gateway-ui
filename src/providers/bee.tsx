3f897f3970110b1281ab08c8b812557ba15f1544
Attila Gazso <agazso@gmail.com>
2021-06-29 15:31:25 +0200

    feat: parallel download, maxTries: 1

--- src/providers/bee.tsx
+++ src/providers/bee.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactChild, ReactElement } from 'react'
 import { Bee, Data, FileData, Reference } from '@ethersphere/bee-js'
 import { BEE_HOSTS, META_FILE_NAME, POSTAGE_STAMP, PREVIEW_FILE_NAME } from '../constants'
 
+const MAX_TRIES = 1
 const bees = BEE_HOSTS.map(host => new Bee(host))
 const randomBee = bees[Math.floor(Math.random() * bees.length)]
 
@@ -35,38 +36,70 @@ function sleep(ms: number): Promise<void> {
   return new Promise<void>(resolve => setTimeout(() => resolve(), ms))
 }
 
-async function race<T>(promises: Promise<T>[]): Promise<T> {
+interface WithBee {
+  bee: Bee
+}
+
+type Action<T extends WithBee> = () => Promise<T>
+
+async function race<T extends WithBee>(actions: Action<T>[], options?: { maxTries?: number }): Promise<T> {
   let canceled = false
-  const safePromise = async (promise: Promise<T>) => {
+  const safeAction = async (action: Action<T>) => {
+    let numTries = 0
     while (!canceled) {
+      if (typeof options?.maxTries === 'number' && numTries >= options.maxTries) {
+        return
+      }
       try {
-        const result = await promise
+        numTries++
+        const result = await action()
         canceled = true
 
         return result
       } catch (e) {
-        await sleep(Math.random() * 10 * 1000)
+        if (canceled) {
+          return
+        }
+        await sleep(Math.random() * 20 * 1000)
       }
     }
   }
 
-  const result = await Promise.race(promises.map(safePromise))
+  const result = await Promise.race(actions.map(safeAction))
 
   if (!result) throw new Error('race failed')
 
   return result
 }
 
-function tryDownloadFileFromAll(hash: Reference | string, path?: string) {
-  const actions = bees.map(bee => bee.downloadFile(hash, path))
+async function downloadFile(bee: Bee, hash: Reference | string, path?: string) {
+  const response = await bee.downloadFile(hash, path)
 
-  return race(actions)
+  return {
+    ...response,
+    bee,
+  }
+}
+
+function tryDownloadFileFromAll(hash: Reference | string, path?: string) {
+  const actions = bees.map(bee => () => downloadFile(bee, hash, path))
+
+  return race(actions, { maxTries: MAX_TRIES })
+}
+
+async function downloadData(bee: Bee, hash: Reference | string) {
+  const response = await bee.downloadData(hash)
+
+  return {
+    ...response,
+    bee,
+  }
 }
 
 function tryDownloadDataFromAll(hash: Reference | string) {
-  const actions = bees.map(bee => bee.downloadData(hash))
+  const actions = bees.map(bee => () => downloadData(bee, hash))
 
-  return race(actions)
+  return race(actions, { maxTries: MAX_TRIES })
 }
 
 export function Provider({ children }: Props): ReactElement {
