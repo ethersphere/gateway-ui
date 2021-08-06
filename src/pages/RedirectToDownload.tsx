9718d6dfcac10cb0f3fee9c92cb7357da57754d3
Vojtech Simetka <vojtech@simetka.cz>
2021-08-06 10:21:44 +0200

    feat: redirect /bzz/<hash> requests to direct download (#105)
    * feat: redirect /bzz/<hash> requests to direct download
    
    * chore: use window.location.replace instead of assign

---
+++ src/pages/RedirectToDownload.tsx
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom'
+import { DIRECT_DOWNLOAD_URL } from '../constants'
+
+const RedirectToDownload = (): null => {
+  const { hash } = useParams<{ hash: string }>()
+
+  // React router can redirect only withing the app itself, that is why we need to assing window.location
+  // https://knowbody.github.io/react-router-docs/api/Redirect.html
+  window.location.replace(`${DIRECT_DOWNLOAD_URL}${hash}`)
+
+  return null
+}
+
+export default RedirectToDownload
