22bf0e62d23ba311395ed4159ee8d47339576ffd
Vojtech Simetka <vojtech@simetka.cz>
2021-11-06 16:45:58 +0100

    chore: added depcheck

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,9 @@
 import type { ReactElement } from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
+// pages
+import { Access, AccessHash, LandingPage, Share, Page404, TermsAndConditions, RedirectToDownload } from './pages'
+
 export const LANDING_PAGE = '/'
 export const ACCESS = '/access'
 export const ACCESS_HASH = (hash = ':hash'): string => `/access/${hash}`
@@ -9,9 +12,6 @@ export const TERMS_AND_CONDITIONS = '/termsandconditions'
 
 // ENS compatibility
 export const BZZ = '/bzz/:hash'
-
-// pages
-import { Access, AccessHash, LandingPage, Share, Page404, TermsAndConditions, RedirectToDownload } from './pages'
 
 const BaseRouter = (): ReactElement => (
   <BrowserRouter>
